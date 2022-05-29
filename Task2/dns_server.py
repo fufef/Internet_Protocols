d3a975ace68693f841af6836822eb65d0291a038
fufef <malenkiymayonezic@gmail.com>
2022-05-29 23:31:34 +0500

    fixed ptr & errors

--- Task2/dns_server.py
+++ Task2/dns_server.py
@@ -1,10 +1,15 @@
 import socket
-import timer
 from datetime import datetime, timedelta
-
 import dns_structure
 
-ROOT_SERVER_IP = "198.41.0.4"
+root = "198.41.0.4"
+errors = {
+    1: "Cannot understand query structure! ",
+    2: "Server Failure! ",
+    3: "Non-existent domain! ",
+    4: "Cannot execute query of this type! ",
+    5: "Security error! "
+}
 
 
 def receive_data(server, data_from_client):
@@ -15,9 +20,8 @@ def receive_data(server, data_from_client):
         data_from_root = dns_structure.DnsPackage.unpack(
             sock_for_root.recvfrom(1024)[0])
     except socket.timeout:
-        print("Timeout was 2 seconds. Проверьте подключение к интернету.")
+        print("Timeout was 2 seconds. Please check your Internet connection.")
         return None
-
     return data_from_root
 
 
@@ -25,10 +29,8 @@ def main():
     port = 53
     host = "127.0.0.1"
     cache = dict()
-
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((host, port))
-
     while True:
         try:
             data, address = sock.recvfrom(1024)
@@ -36,60 +38,65 @@ def main():
                 data_from_client = dns_structure.DnsPackage.unpack(data)
                 rcode = data_from_client.header.rcode
                 sock_for_root = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
                 ans = []
                 add = []
                 auth = []
                 for q in data_from_client.questions:
                     if q in cache:
                         q_ans, q_add, q_auth, time_limit = cache[q]
-
                         if datetime.now() < time_limit:
                             ans += q_ans
                             add += q_add
                             auth += q_auth
                             continue
-
                     pack_to_root = dns_structure.DnsPackage(
                         data_from_client.header, [q], [], [], []).pack()
 
-                    sock_for_root.sendto(pack_to_root, (ROOT_SERVER_IP, 53))
+                    sock_for_root.sendto(pack_to_root, (root, 53))
                     sock_for_root.settimeout(2)
                     try:
-                        data_from_root = dns_structure.DnsPackage.unpack(
-                            sock_for_root.recvfrom(1024)[0])
+                        data_from_root = dns_structure.DnsPackage.unpack(sock_for_root.recvfrom(1024)[0])
                     except socket.timeout:
-                        print("Timeout was 2 seconds. Проверьте подключение к интернету.")
+                        print("Timeout was 2 seconds. Please check your Internet connection.")
                         break
 
                     d = data_from_root
                     while d.header.ancount == 0:
+                        rcode = d.header.rcode
+
+                        if rcode:
+                            print(
+                                errors[rcode],
+                                "Question was: name=", q.qname, ", type=",
+                                q.qtype, ", class=", q.qclass, sep="")
+                            break
+
                         r = list(filter(lambda x: x.type == 1, d.additionals))
 
                         if len(r) > 0:
                             next_ip = '.'.join(map(str, r[0].rdata))
                         else:
-                            r = list(filter(lambda x: x.type == 2, d.authorities))
+                            r = list(
+                                filter(lambda x: x.type == 2, d.authorities))
 
-                            if len(r) == 0:
-                                rcode = 3
-                                print("Non-existent domain! Question was: name=", q.qname, ", type=", q.qtype, ", class=",
-                                      q.qclass, sep="")
+                            if not r:
                                 break
 
                             domain = dns_structure.get_url(0, r[0].rdata)[0] \
                                 .decode()
-                            next_ip = ROOT_SERVER_IP
+                            next_ip = root
 
                             while d.header.ancount == 0:
                                 sub_q = dns_structure.Question(domain, 1, 1)
                                 pack = dns_structure.DnsPackage(
-                                    data_from_client.header, [sub_q], [], [], [])
+                                    data_from_client.header, [sub_q], [], [],
+                                    [])
 
                                 d = receive_data(next_ip, pack)
 
                                 r = list(
-                                    filter(lambda x: x.type == 1, d.additionals))
+                                    filter(lambda x: x.type == 1,
+                                           d.additionals))
                                 next_ip = '.'.join(map(str, r[0].rdata))
 
                             next_ip = '.'.join(map(str, d.answers[0].rdata))
@@ -97,7 +104,8 @@ def main():
                         d = receive_data(next_ip, data_from_client)
 
                     ttl = min((x.ttl
-                               for x in d.answers + d.additionals + d.authorities),
+                               for x in
+                               d.answers + d.additionals + d.authorities),
                               default=0)
 
                     if rcode == 0:
@@ -121,6 +129,7 @@ def main():
                 sock.sendto(a, address)
         except Exception:
             pass
+
 
 if __name__ == '__main__':
     main()
