4b3659f3a5bb9f70988c6fee8b62f2201fb787dc
fufef <malenkiymayonezic@gmail.com>
2022-07-02 17:45:36 +0500

    added rus text, long subject, mime dictionary

--- Task3/smtp.py
+++ Task3/smtp.py
@@ -2,72 +2,77 @@ import base64
 import socket
 import ssl
 from pathlib import Path
+from settings import USER_NAME, PASSWORD, HOST_ADDR, PORT, RECIPIENT
 
-from settings import *
+mime_dict = {'image': ['image/jpeg', 'image/webp', 'image/bmp', 'image/png', 'image/gif'],
+             'text': ['text/plain', 'text/html', 'text/css', 'text/calendar', 'text/javscript'],
+             'video': ['video/mpeg'],
+             'application': ['application/json', 'application/gzip']}
 
 
-def request(sock, request):
-    sock.send((request + '\n').encode())
+def request(sock, req):
+    sock.send((req + '\n').encode())
     recv_data = sock.recv(65535).decode()
     return recv_data
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-    client.connect((host_addr, port))
-    client = ssl.wrap_socket(client)
-    print(client.recv(1024).decode())
-    print(request(client, 'ehlo myUserName'))
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect((HOST_ADDR, PORT))
+        client = ssl.wrap_socket(client)
+        client.recv(1024).decode()
+        request(client, 'ehlo myUserName')
 
-    base64login = base64.b64encode(user_name.encode()).decode()
-    base64password = base64.b64encode(password.encode()).decode()
+        base64login = base64.b64encode(USER_NAME.encode()).decode()
+        base64password = base64.b64encode(PASSWORD.encode()).decode()
 
-    print(request(client, 'AUTH LOGIN'))
-    print(request(client, base64login))
-    print(request(client, base64password))
+        request(client, 'AUTH LOGIN')
+        request(client, base64login)
+        request(client, base64password)
+        request(client, f'MAIL FROM:{USER_NAME}')
+        request(client, f"RCPT TO:{RECIPIENT}")
+        request(client, 'DATA')
 
-    print(request(client, 'MAIL FROM:IvanTestMail3@yandex.ru'))
-    print(request(client, "RCPT TO:IvanTestMail3@yandex.ru"))
-    print(request(client, 'DATA'))
+        msg = ''
 
-    msg = ''
-
-    with open('headers.txt') as f:
-        msg += f.read()
-
-    msg += '\n'
-
-    bound = '--my-bound-mix'
-
-    msg += 'Content-Type: multipart/mixed;' + \
-           f'boundary="{bound}"'
-
-    msg += '\n\n'
-    msg += f'--{bound}\n'
-    msg += 'Content-Type: text/plain\n\n'
-
-    with open('msg.txt') as file:
-        msg += file.read() + '\n\n'
-
-    for f in Path('./attachments').iterdir():
-        msg += f'--{bound}\n'
-
-        msg += 'Content-Disposition: attachment;\n' + \
-               f' filename="{f.name}"\n'
-        msg += 'Content-Transfer-Encoding: base64\n'
-        msg += 'Content-Type: image/jpeg'
-        msg += '\n\n'
-
-        with f.open('rb') as f1:
-            msg += base64.b64encode(f1.read()).decode()
+        with open('headers.txt', 'r', encoding='utf-8') as f:
+            msg += f.read()
 
         msg += '\n'
 
-    msg += f'--{bound}--'
-    msg += '\n.\n'
+        bound = '--my-bound-mix'
 
-    print(msg)
+        msg += 'Content-Type: multipart/mixed;' + \
+               f'boundary="{bound}"'
 
-    print(request(client, msg))
+        msg += '\n\n'
+        msg += f'--{bound}\n'
+        msg += f'Content-Type: {mime_dict["text"][0]}\n\n'
 
-    # TODO: MIME из словаря, русский subject (очень длинный),
-    # обработка ошибок сети, обработка откликов сервера
+        with open('msg.txt', 'r', encoding='utf-8') as file:
+            msg += file.read() + '\n\n'
+
+        for f in Path('./attachments').iterdir():
+            msg += f'--{bound}\n'
+
+            msg += 'Content-Disposition: attachment;\n' + \
+                   f' filename="{f.name}"\n'
+            msg += 'Content-Transfer-Encoding: base64\n'
+            msg += f'Content-Type: {mime_dict["image"][0]}'
+            msg += '\n\n'
+
+            with f.open('rb') as f1:
+                msg += base64.b64encode(f1.read()).decode()
+
+            msg += '\n'
+
+        msg += f'--{bound}--'
+        msg += '\n.\n'
+
+        print(msg)
+
+        request(client, msg)
+
+
+if __name__ == '__main__':
+    main()
